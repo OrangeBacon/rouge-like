5dcc31975ea204ab864efc65ee4d2cdbaf691570
ScratchOs <Computer.backup.15@gmail.com>
2017-12-23 18:03:21 +0000

    add background to loaded screen

--- states/loaded.lua
+++ states/loaded.lua
@@ -6,19 +6,24 @@ require "gui.button"
 local Loading = GameState:extend();
 
 function Loading:init()
-
+  self.splash = love.graphics.newImage("splash.png")
 end
 
 function Loading:update(dt)
   gui.label("Loaded!", 50, 50)
   gui:updateTab()
 
-  if gui.button("Quit", 50, 100).hit then
+  if gui.button("Quit", 50, 100, love.graphics.getWidth() - 100, 100).hit then
     love.event.quit()
   end
 end
 
 function Loading:draw()
+  local sx = love.graphics.getWidth() / self.splash:getWidth()
+  local sy = love.graphics.getHeight() / self.splash:getHeight()
+  local scale = math.max(sx, sy)
+
+  love.graphics.draw(self.splash, 0, 0, 0, scale, scale)
 	gui:draw()
 end
 
